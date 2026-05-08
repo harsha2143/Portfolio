7a2cadc447d9b7ea3affdf10ad953ef82945b8f0
harsha2143 <harshavardhansaripalli21@gamil.com>
2026-05-08 13:17:38 +0530

    update blog section

--- src/components/Blog.jsx
+++ src/components/Blog.jsx
@@ -14,8 +14,8 @@ export default function Blog() {
                         Latest Blogs
                     </p>
 
-                    <h2 className="text-4xl md:text-5xl font-bold leading-tight">
-                        Thoughts, tutorials & learnings
+                    <h2 className="text-3xl md:text-4xl font-bold leading-tight">
+                        Thoughts, tutorials <span className="text-blue-500">& learnings</span> 
                     </h2>
 
                     <p className="text-gray-400 mt-4 text-lg max-w-3xl">
@@ -44,7 +44,7 @@ export default function Blog() {
                             </div> */}
 
                             {/* Title */}
-                            <h3 className="text-2xl font-bold leading-snug transition-all duration-300 group-hover:text-blue-400 flex items-center justify-between mb-2">
+                            <h3 className="text-xl font-bold leading-snug transition-all duration-300 group-hover:text-blue-400 flex items-center justify-between mb-2">
                                 {blog.title}
                                 <span className="text-blue-300 text-2xl transition-transform duration-300 group-hover:translate-x-1 mb-2">
                                     →
